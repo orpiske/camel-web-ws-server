b1a9e122a3ce7bca4aff68e1045431bbe8c88f05
Otavio R. Piske <angusyoung@gmail.com>
2014-10-22 13:53:28 -0200

    Code cleanup: removed unused import

--- src/main/java/net/orpiske/camel/cxf/web/example/routes/TimeServiceRoute.java
+++ src/main/java/net/orpiske/camel/cxf/web/example/routes/TimeServiceRoute.java
@@ -3,7 +3,6 @@ package net.orpiske.camel.cxf.web.example.routes;
 import net.orpiske.camel.cxf.web.example.processors.TimeServiceProcessor;
 import net.orpiske.examples.cxf.webservice.TimeService;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.log4j.Logger;
 
 /**
  * Created by orpiske on 20/10/14.
