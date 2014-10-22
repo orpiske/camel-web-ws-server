ed2a275a039c4886f3eb9ed2f51fbdcdc7af2bfc
Otavio R. Piske <angusyoung@gmail.com>
2014-10-22 13:22:48 -0200

    Modified the code to throw exceptions if unable to obtain/convert current date/time

--- src/main/java/net/orpiske/camel/cxf/web/example/bean/TimeServiceBean.java
+++ src/main/java/net/orpiske/camel/cxf/web/example/bean/TimeServiceBean.java
@@ -1,5 +1,6 @@
 package net.orpiske.camel.cxf.web.example.bean;
 
+import net.orpiske.camel.cxf.web.example.exceptions.TimeServiceException;
 import org.apache.log4j.Logger;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -14,8 +15,8 @@ import java.util.GregorianCalendar;
 public class TimeServiceBean {
 	private static final Logger logger = Logger.getLogger(TimeServiceBean.class);
 	
-	public XMLGregorianCalendar timeService() {
-		logger.info("Processing a new time service request");
+	public XMLGregorianCalendar timeService() throws TimeServiceException {
+		logger.info("Obtaining current time");
 		
 		GregorianCalendar c = new GregorianCalendar();
 		c.setTime(new Date());
@@ -24,10 +25,7 @@ public class TimeServiceBean {
 
 			return ret;
 		} catch (DatatypeConfigurationException e) {
-
-			e.printStackTrace();
+			throw new TimeServiceException("Unable to obtain current time", e);
 		}
-
-		return null;
 	}
 }
