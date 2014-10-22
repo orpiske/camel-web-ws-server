dcfd527f88656617a71096c0de9c7db4ab06a70e
Otavio R. Piske <angusyoung@gmail.com>
2014-10-22 13:22:11 -0200

    Added a new exception type, to handle problems in the bean

---
+++ src/main/java/net/orpiske/camel/cxf/web/example/exceptions/TimeServiceException.java
@@ -0,0 +1,10 @@
+package net.orpiske.camel.cxf.web.example.exceptions;
+
+/**
+ * Created by orpiske on 22/10/14.
+ */
+public class TimeServiceException extends Exception {
+	public TimeServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
